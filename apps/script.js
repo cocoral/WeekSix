f71667655495f30e93aa75fb8e9e34611d8b5c15
Xinting He <hxth2018@gmail.com>
2016-02-23 17:21:21 -0500

    psudo-code and some html content

---
+++ apps/script.js
@@ -0,0 +1,15 @@
+// Set up Jquery clock plugin -- Flipclock.js (Get time)
+
+// Get time from clock and store in variable (breakfast - lunch - dinner)
+
+// Get value from check-box ( Allergies ) ( Diet restrictions )
+
+// $Ajax call - Get information from Yummly API
+
+// Print on thml
+
+// Print button -send to printer
+
+// Add more function 
+
+// Light box for about content 
