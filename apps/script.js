59afc9609185672eef454a95c539c209e06e204f
Laura Culham <culham.laura@gmail.com>
2016-02-24 19:12:39 -0500

    lots and lots of shit done

--- apps/script.js
+++ apps/script.js
@@ -1,3 +1,4 @@
+'use strict';
 
 // Set up Jquery clock plugin -- Flipclock.js (Get time)
 
@@ -11,17 +12,96 @@
 
 // Print button -send to printer
 
-// Add more function 
+// Add more function
 
-// Light box for about content 
+// Light box for about content
 
 var coolApp = {};
 
-coolApp.appId = ac5ae063;
+coolApp.appId = '9a82c4a1';
 
-coolApp.appKey = cfec60ec11e2efaa7f513383b9940b85;
+coolApp.appKey = 'd750f8a3c48c097b49c0082762f6a0ae';
 
+var mealtime = '';
 
-//ok
+var allergy = [];
 
+var diet = [];
 
+coolApp.init = function () {
+	//code to start app goes in here
+	coolApp.gettime();
+	coolApp.getinputs();
+	
+};
+
+coolApp.gettime = function () {
+	//code to gather info on time and inputs
+	// var currentTime = $.now();
+	// console.log(currentTime);
+	var dt = new Date();
+	var time = dt.getHours();
+	console.log(time);
+	if (time < 5) {
+		// console.log('works');
+		mealtime = 'pub';
+	} else if (time >= 5 && time < 11) {
+		mealtime = 'breakfast';
+	} else if (time >= 11 && time < 16) {
+		mealtime = 'lunch';
+	} else if (time >= 16) {
+		mealtime = 'dinner';
+	};
+};
+
+// coolApp.gettime();
+
+coolApp.getinputs = function () {
+	$('.whatToEat').submit(function (e) {
+		allergy = [];
+		e.preventDefault();
+		$('.Allergies input:checked').each(function (i, event) {
+			allergy.push(event.value);
+		});
+
+		allergy = allergy.join(' ');
+		console.log(allergy);
+		diet = [];
+		$('.diet input:checked').each(function (i, event) {
+			diet.push(event.value);
+		});
+		diet = diet.join(' ');
+		console.log(diet);
+		coolApp.getRecipe(allergy,diet);
+	});
+
+};
+
+// coolApp.getinputs();
+
+coolApp.getRecipe = function (allergy,diet) {
+	console.log(mealtime);
+	console.log(allergy, diet);
+	$.ajax({
+		url: 'http://api.yummly.com/v1/api/recipes?_app_id=9a82c4a1&_app_key=d750f8a3c48c097b49c0082762f6a0ae',
+		type: 'GET',
+		dataType: 'jsonp',
+		data: {
+			requirePictures: true,
+			q: mealtime,
+			'allowedAllergy[]': allergy,
+			'allowedDiet[]': diet
+		}
+
+	}).then(function (res) {
+		console.log(res);
+	});
+};
+
+// coolApp.getRecipe();
+
+coolApp.displayRecipes = function () {};
+
+$(function () {
+	coolApp.init();
+});
